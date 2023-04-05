f0a6818f6086c4e309d59574c8f087ab01eae2c6
vinoku <vinoku78@gmail.com>
2023-04-05 19:11:15 +0200

    First child component

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        "brand-grey-1": "#dadce0",
+        "brand-blue-1": "#1967d2",
+        "brand-green-1": "#137333"
       }
     }
   },
