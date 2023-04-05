a09b1486588dcad1dda4bda470e88c003bbe57de
vinoku <vinoku78@gmail.com>
2023-04-05 20:30:46 +0200

    Setup unit tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,18 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution")
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
   extends: [
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
-    "@vue/eslint-config-prettier/skip-formatting"
+    "@vue/eslint-config-prettier/skip-formatting",
+    "plugin:vitest-globals/recommended"
   ],
   parserOptions: {
     ecmaVersion: "latest"
+  },
+  env: {
+    "vitest-globals/env": true
   }
-}
+};
