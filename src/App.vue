8c0168de9a751b5eff5d10c0e315fa6c3cca455e
vinoku <vinoku78@gmail.com>
2023-04-05 15:51:04 +0200

    Initial setup vue app

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="wrapper">
+    <HelloWorld msg="You did it!" />
+    <HelloWorld msg="Nice job!" />
+  </div>
+</template>
